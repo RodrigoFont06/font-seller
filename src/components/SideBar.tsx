2fc44c9f258588a24a1af6d982206fd2cfdefcef
RodrigoFont06 <rofont06@gmail.com>
2026-04-06 19:16:08 -0300

    feat: add Firebase dependency, create sidebar component and styles

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,15 @@
+import { useState} from "react";
+
+
+export default function SideBar() {
+    
+    return(
+        <aside>
+            <h2>Categories</h2>
+            <ul>
+                
+
+            </ul>
+        </aside>
+    )
+}

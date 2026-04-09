e33f2a9ce0e8e998c617df1489712a0532766fb4
RodrigoFont06 <rofont06@gmail.com>
2026-04-09 15:24:33 -0300

    feat: add SideBar component to display product categories from Firestore

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/header.js';
+import SideBar from './components/SideBar.js';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header title="Font Seller" />
+      <SideBar />
     </>
   )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,15 +1,34 @@
-import { useState} from "react";
-
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase';
+import { useState, useEffect } from "react";
 
 export default function SideBar() {
-    
-    return(
-        <aside>
-            <h2>Categories</h2>
-            <ul>
-                
+    const [categories, setCategories] = useState<string[]>([]);
 
-            </ul>
+    useEffect(() => {
+        const fetchCategories = async () => {
+            const categoriesCollection = collection(db, "products");
+            const categoriesSnapshot = await getDocs(categoriesCollection);
+            const categoriesList = categoriesSnapshot.docs.map(doc => doc.data().category);
+            setCategories(categoriesList);
+        };
+
+        fetchCategories();
+    }, []);
+
+    return (
+        <aside className='bg-black/40 backdrop-blur-[1px] flex justify-center items-baseline h-screen w-1/8 shadow p-4'>
+            <div>
+                <h2 className='text-white font-bold text-2xl'>Categories</h2>
+                <ul className="">
+                    <li>
+                        
+                    </li>
+                </ul>
+            </div>
+            <div>
+
+            </div>
         </aside>
     )
 }

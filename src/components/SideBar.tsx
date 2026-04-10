12514c077055af603af7971cb9b9fdf9f8675fef
RodrigoFont06 <rofont06@gmail.com>
2026-04-10 10:31:16 -0300

    feat: enhance SideBar component with toggle functionality for categories

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 
 export default function SideBar() {
     const [categories, setCategories] = useState<string[]>([]);
+    const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         const fetchCategories = async () => {
@@ -18,11 +19,11 @@ export default function SideBar() {
 
     return (
         <aside className='bg-[#5E778C]/30 backdrop-blur-[1px] flex justify-center items-baseline h-screen w-1/8 shadow p-4'>
-            <div>
+            <div className='flex flex-col gap-4'>
                 <h2 className='text-white font-bold text-2xl'>Categories</h2>
                 <ul className="">
                     <li>
-                        
+                        <button className="text-white hover:text-blue-300 w-full text-left" onClick={() => setIsOpen(!isOpen)}>Components {isOpen ? '-' : '+'}</button>
                     </li>
                 </ul>
             </div>
